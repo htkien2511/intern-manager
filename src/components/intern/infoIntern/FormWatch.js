977f44e6584c5641d7247e2a03626a335b4ff87c
TrongDuc99 <ducphan.24hdev@gmail.com>
2021-04-01 21:08:08 +0700

    remake profile user

--- src/components/intern/infoIntern/FormWatch.js
+++ src/components/intern/infoIntern/FormWatch.js
@@ -3,21 +3,21 @@ import logo from "../../../assets/images/logo.png";
 import { NavLink } from "react-router-dom";
 import { ROUTE_EDIT_PROFILE } from "../../../utils/routes";
 function FormWatch() {
-  return(
+    return (
         <div className="form-watch">
             <h2>Intern's Profile</h2>
             <div className="form-watch__body">
                 <div className="form-watch__body__info">
-                    <div className="info_avatar">
-                        <img src={logo} className="avatar" alt="avatar"/>
-                    </div>  
+                    <div className="info_avatar align__center">
+                        <img src={logo} className="avatar" alt="avatar" />
+                    </div>
                     <form>
                         <div className="info__name">
                             <label>Full name:</label>
                             <p>Phan Gia Sang</p>
-                        </div>      
-                        <div className="info__email">   
-                            <label>Email:</label>  
+                        </div>
+                        <div className="info__email">
+                            <label>Email:</label>
                             <p>sang@gmail.com</p>
                         </div>
                         <div className="info__department">
@@ -39,8 +39,8 @@ function FormWatch() {
                 </div>
             </div>
         </div>
-        
-  );
+
+    );
 }
 
 export default FormWatch;
