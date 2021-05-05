c13a06478f443d78df9f347748b9fa9d9052e255
TrongDuc99 <ducphan.24hdev@gmail.com>
2021-05-06 01:27:41 +0700

    init chart

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import HomePage from "./containers/HomePage"
+import HomePage from "./containers/HomePage";
 import withAuthUser from "./HOCS/intern/withAuthUser";
 import { withAuth, withAuthLayout } from "./HOCS";
 import {
-  ROUTE_FORGOTPASSWORD, ROUTE_LOGIN, ROUTE_MANAGE_INTERN, ROUTE_REGISTER, ROUTE_RESETPASSWORD, ROUTE_ADMIN_LOGIN,
-  ROUTE_MANAGE_SCHEDULE, ROUTE_MANAGE_SCHEDULE_DETAIL, ROUTE_MANAGE_LEADER, ROUTE_MANAGE_TASK, ROUTE_MANAGE_ACCOUNT_WAITING,
-  ROUTE_PROFILE, ROUTE_EDIT_PROFILE, ROUTE_CONVERSATION, ROUTE_FEEDBACKS, ROUTE_REGISTER_SCHEDULE, ROUTE_TASK_MANAGEMENT, ROUTE_SEND_FEEDBACK, ROUTE_VIEW_STATISTIC, ROUTE_MANAGE_FEEDBACK__INTERN, ROUTE_MANAGE_TASK_DETAIL, ROUTE_MANAGE_FEEDBACK__INTERN_DETAIL
+  ROUTE_FORGOTPASSWORD,
+  ROUTE_LOGIN,
+  ROUTE_MANAGE_INTERN,
+  ROUTE_REGISTER,
+  ROUTE_RESETPASSWORD,
+  ROUTE_ADMIN_LOGIN,
+  ROUTE_MANAGE_SCHEDULE,
+  ROUTE_MANAGE_SCHEDULE_DETAIL,
+  ROUTE_MANAGE_LEADER,
+  ROUTE_MANAGE_TASK,
+  ROUTE_MANAGE_ACCOUNT_WAITING,
+  ROUTE_PROFILE,
+  ROUTE_EDIT_PROFILE,
+  ROUTE_CONVERSATION,
+  ROUTE_FEEDBACKS,
+  ROUTE_REGISTER_SCHEDULE,
+  ROUTE_TASK_MANAGEMENT,
+  ROUTE_SEND_FEEDBACK,
+  ROUTE_VIEW_STATISTIC,
+  ROUTE_MANAGE_FEEDBACK__INTERN,
+  ROUTE_MANAGE_TASK_DETAIL,
+  ROUTE_MANAGE_FEEDBACK__INTERN_DETAIL,
 } from "./utils/routes";
 import { ForgotPassword } from "./containers/forgotPassword";
 import { ResetPassword } from "./containers/resetPassword";
@@ -20,11 +39,21 @@ import Feedback from "./containers/intern/feedback/Feedback";
 import SendFeedback from "./containers/intern/feedback/SendFeedback";
 import Conversation from "./containers/intern/feedback/Conversation";
 import Calendar from "./containers/intern/calendar/Calendar";
-import { ManageSchedule, ManageScheduleDetail } from "./containers/admin/manageScheduleIntern";
-import { ManageTask, ManageTaskDetail } from "containers/admin/manageTaskIntern";
+import {
+  ManageSchedule,
+  ManageScheduleDetail,
+} from "./containers/admin/manageScheduleIntern";
+import {
+  ManageTask,
+  ManageTaskDetail,
+} from "containers/admin/manageTaskIntern";
 import { ManageAccountWaiting } from "containers/admin/manageAccountWaiting";
-import { ManageFeedback, ManageFeedbackDetail } from "containers/admin/manageFeedback";
-import {ManageLeader} from "containers/admin/manageAccountLeader";
+import {
+  ManageFeedback,
+  ManageFeedbackDetail,
+} from "containers/admin/manageFeedback";
+import { ManageLeader } from "containers/admin/manageAccountLeader";
+import { ManageViewStatistic } from "containers/admin/manageViewStatistic.js";
 
 function App() {
   return (
@@ -32,28 +61,112 @@ function App() {
       <Switch>
         <Route path="/" component={withAuthLayout(HomePage)} exact />
         <Route path={ROUTE_LOGIN} component={withAuthLayout(Login)} exact />
-        <Route path={ROUTE_ADMIN_LOGIN} component={withAuthLayout(Login)} exact />
-        <Route path={ROUTE_REGISTER} component={withAuthLayout(Register)} exact />
-        <Route path={ROUTE_FORGOTPASSWORD} component={withAuthLayout(ForgotPassword)} exact />
-        <Route path={ROUTE_RESETPASSWORD} component={withAuthLayout(ResetPassword)} exact />
-        <Route path={ROUTE_PROFILE} component={withAuthUser(InfoIntern)} exact />
-        <Route path={ROUTE_EDIT_PROFILE} component={withAuthUser(EditProfile)} exact />
-        <Route path={ROUTE_TASK_MANAGEMENT} component={withAuthUser(TaskManagement)} exact />
-        <Route path={ROUTE_FEEDBACKS} component={withAuthUser(Feedback)} exact />
-        <Route path={ROUTE_SEND_FEEDBACK} component={withAuthUser(SendFeedback)} exact />
-        <Route path={ROUTE_CONVERSATION} component={withAuthUser(Conversation)} exact />
-        <Route path={ROUTE_REGISTER_SCHEDULE} component={withAuthUser(Calendar)} exact />
+        <Route
+          path={ROUTE_ADMIN_LOGIN}
+          component={withAuthLayout(Login)}
+          exact
+        />
+        <Route
+          path={ROUTE_REGISTER}
+          component={withAuthLayout(Register)}
+          exact
+        />
+        <Route
+          path={ROUTE_FORGOTPASSWORD}
+          component={withAuthLayout(ForgotPassword)}
+          exact
+        />
+        <Route
+          path={ROUTE_RESETPASSWORD}
+          component={withAuthLayout(ResetPassword)}
+          exact
+        />
+        <Route
+          path={ROUTE_PROFILE}
+          component={withAuthUser(InfoIntern)}
+          exact
+        />
+        <Route
+          path={ROUTE_EDIT_PROFILE}
+          component={withAuthUser(EditProfile)}
+          exact
+        />
+        <Route
+          path={ROUTE_TASK_MANAGEMENT}
+          component={withAuthUser(TaskManagement)}
+          exact
+        />
+        <Route
+          path={ROUTE_FEEDBACKS}
+          component={withAuthUser(Feedback)}
+          exact
+        />
+        <Route
+          path={ROUTE_SEND_FEEDBACK}
+          component={withAuthUser(SendFeedback)}
+          exact
+        />
+        <Route
+          path={ROUTE_CONVERSATION}
+          component={withAuthUser(Conversation)}
+          exact
+        />
+        <Route
+          path={ROUTE_REGISTER_SCHEDULE}
+          component={withAuthUser(Calendar)}
+          exact
+        />
         {/* Admin */}
-        <Route path={ROUTE_MANAGE_INTERN} component={withAuth(ManageIntern, false)} exact />
-        <Route path={ROUTE_MANAGE_SCHEDULE} component={withAuth(ManageSchedule, false)} exact />
-        <Route path={ROUTE_MANAGE_SCHEDULE_DETAIL} component={withAuth(ManageScheduleDetail, false)} exact />
-        <Route path={ROUTE_MANAGE_LEADER} component={withAuth(ManageLeader, false)} exact />
-        <Route path={ROUTE_MANAGE_TASK} component={withAuth(ManageTask, false)} exact />
-        <Route path={ROUTE_MANAGE_TASK_DETAIL} component={withAuth(ManageTaskDetail, false)} exact />
-        <Route path={ROUTE_MANAGE_FEEDBACK__INTERN} component={withAuth(ManageFeedback, false)} exact />
-        <Route path={ROUTE_MANAGE_FEEDBACK__INTERN_DETAIL} component={withAuth(ManageFeedbackDetail, false)} exact />
-        <Route path={ROUTE_MANAGE_ACCOUNT_WAITING} component={withAuth(ManageAccountWaiting, false)} exact />
-        <Route path={ROUTE_VIEW_STATISTIC} component={withAuth(ManageSchedule, false)} exact />
+        <Route
+          path={ROUTE_MANAGE_INTERN}
+          component={withAuth(ManageIntern, false)}
+          exact
+        />
+        <Route
+          path={ROUTE_MANAGE_SCHEDULE}
+          component={withAuth(ManageSchedule, false)}
+          exact
+        />
+        <Route
+          path={ROUTE_MANAGE_SCHEDULE_DETAIL}
+          component={withAuth(ManageScheduleDetail, false)}
+          exact
+        />
+        <Route
+          path={ROUTE_MANAGE_LEADER}
+          component={withAuth(ManageLeader, false)}
+          exact
+        />
+        <Route
+          path={ROUTE_MANAGE_TASK}
+          component={withAuth(ManageTask, false)}
+          exact
+        />
+        <Route
+          path={ROUTE_MANAGE_TASK_DETAIL}
+          component={withAuth(ManageTaskDetail, false)}
+          exact
+        />
+        <Route
+          path={ROUTE_MANAGE_FEEDBACK__INTERN}
+          component={withAuth(ManageFeedback, false)}
+          exact
+        />
+        <Route
+          path={ROUTE_MANAGE_FEEDBACK__INTERN_DETAIL}
+          component={withAuth(ManageFeedbackDetail, false)}
+          exact
+        />
+        <Route
+          path={ROUTE_MANAGE_ACCOUNT_WAITING}
+          component={withAuth(ManageAccountWaiting, false)}
+          exact
+        />
+        <Route
+          path={ROUTE_VIEW_STATISTIC}
+          component={withAuth(ManageViewStatistic, false)}
+          exact
+        />
       </Switch>
     </Router>
   );
---
+++ src/containers/admin/manageViewStatistic.js/index.js
@@ -0,0 +1,2 @@
+import ManageViewStatistic from "./ManageViewStatistic";
+export { ManageViewStatistic };
